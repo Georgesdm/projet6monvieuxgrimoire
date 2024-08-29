1b9b3a162de490e159582d04a9d30567f6caa8d0
georges <118487336+Georgesdm@users.noreply.github.com>
2024-08-29 22:19:57 +0200

    feat: Mise à jour des routes d'authentification pour l'inscription et la connexion

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const authController = require('../controllers/authController');
-const { authenticateToken } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.post('/signup', authController.signUp);
 router.post('/login', authController.login);
-//router.get('/verify', authenticateToken, authController.verifyUser);
 
 module.exports = router;
